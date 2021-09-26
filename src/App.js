72996b6838d5ac34b60aef32432480b85ae0596e
craigjtwigg <81586983+craigjtwigg@users.noreply.github.com>
2021-09-26 11:53:44 +0100

    onsubmit and onchange methods refactored, uuid now only called onsubmit. onSubmit now broken out into seperate smaller methods

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ export default class App extends Component {
 
     this.updateInputState = this.updateInputState.bind(this);
     this.submitInputState = this.submitInputState.bind(this);
+    this.generateId = this.generateId.bind(this);
   }
 
   //onChange function to update the state of the targeted input field//
@@ -38,27 +39,49 @@ export default class App extends Component {
         [`${category}Input`]: {
           ...sectionPreviousState,
           [field]: e.target.value,
-          key: uuidv4(),
         },
         [`${category}Store`]: storedState,
       },
     });
   };
 
-  //onSubmit function to update the stored state of the given section//
+//uuid used to asign a unique ID//
 
-  submitInputState = (e, category) => {
-    e.preventDefault();
+  generateId = (category) => {
     this.setState({
       [category]: {
         [`${category}Input`]: {
           ...this.state[category][`${category}Input`],
+          key: uuidv4(),
         },
-        [`${category}Store`]: this.state[category][`${category}Store`].concat(
-          this.state[category][`${category}Input`]
-        ),
+        [`${category}Store`]: this.state[category][`${category}Store`],
       },
     });
+  };
+
+  //method to store the state of the inputted data, set timeout used to allow for the state to be set by the generateID method first//
+
+  storeCategoryState = (category) => {
+    setTimeout(() => {
+      this.setState({
+        [category]: {
+          [`${category}Input`]: {
+            ...this.state[category][`${category}Input`],
+          },
+          [`${category}Store`]: this.state[category][`${category}Store`].concat(
+            this.state[category][`${category}Input`]
+          ),
+        },
+      });
+    }, 100);
+  };
+
+  //onSubmit function to update the stored state of the given section with a unique ID//
+
+  submitInputState = (e, category) => {
+    e.preventDefault();
+    this.generateId(category);
+    setTimeout(this.storeCategoryState(category), 6000);
 
     return;
   };
